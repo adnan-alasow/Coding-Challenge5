043ab4aa1dbce3a99f4613f35ff12f1d7c1f46a9
Adnan Alasow <mearone.omar@gmail.com>
2023-07-12 23:20:55 -0700

    Coding Challenge 5 - problem one solved

---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/*
+1. Write a program that prompts the user to enter five numbers and stores
+them in an array. Then, output the sum of the numbers to the console
+*/
+
+/*
+let numbers = [];
+
+for(let i = 0; i < 5; i = i + 1){
+  let number = parseInt(prompt(`Enter number ${i + 1}:`));
+  numbers.push(number);
+}
+let sum = 0;
+for (let i = 0; i < numbers.length; i++){
+  sum = sum + numbers[i];
+}
+console.log("Sum of all the numbers: ", sum);
+*/
+
+/*
+2. Write a function called findMax that takes in an array of numbers as a parameter and returns the largest number in the array.
+*/
+
+function findMax(array){
+let arrayInput = [];
+
+
+return arrayInput;
+}
